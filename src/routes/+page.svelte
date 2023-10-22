febaa4733b33b085c3aed6c3db19965e679ddebd
razzle <harry@razzle.cloud>
2023-10-22 17:40:48 -0500

    add daisyui
    Signed-off-by: razzle <harry@razzle.cloud>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h1 class="text-3xl font-bold underline">
+    hello world!
+</h1>
+
+<button class="btn">Primary</button>
+<button class="btn btn-outline btn-secondary">Secondary</button>
+<button class="btn btn-outline btn-accent">Accent</button>
